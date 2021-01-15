c40354e0578f89da6eb461b7866c0369350b6d4d
Hambert <hermann.schohaus@posteo.de>
2021-01-15 20:08:11 +0100

    Check if no suitable predictions

--- dailyPredict.py
+++ dailyPredict.py
@@ -1,14 +1,18 @@
 import datetime
 from sendMail import sendMail
-import getPredictions 
+import getPredictions as bp
 import os.path
 from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
-	now = datetime.datetime.now()
-	today = now.strftime("%Y-%m-%d")
+    now = datetime.datetime.now()
+    today = now.strftime("%Y-%m-%d")
 
-	preds = getPredictions.fetch_bet_predictions(writeToFile=False, date=today, apiKey=os.getenv("API-KEY"))
-	
-	sendMail(mailBody = getPredictions.getFilteredPredictions( preds=preds, expired=False ), mailSubject ='Match Prediction for '+ today )
+    preds = bp.fetch_bet_predictions(writeToFile=False, date=today, apiKey=os.getenv("API-KEY"))
+    filteredPred = bp.getFilteredPredictions(preds=preds, expired=False)
+
+    if filteredPred != "":
+        sendMail( mailBody = filteredPred, mailSubject ='Match predictions for '+ today)
+    else:
+        print("No suitable predictions")
